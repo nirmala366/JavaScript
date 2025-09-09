9da5236dc2c427e24f3a94ffc8f3fc35c9bcfc1b
Nirmala <nirmala8579@gmail.com>
2025-09-09 05:54:04 +0530

    03.js

---
+++ 03.js
@@ -0,0 +1,16 @@
+// This is a single-line comment
+let name = "Gowda"; // store your name
+console.log(name);
+
+// Output---
+Gowda
+
+/*
+ This is a multi-line comment.
+ You can write notes across many lines.
+*/
+let age = 20;
+console.log(age);
+
+//Output---
+20
