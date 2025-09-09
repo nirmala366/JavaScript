67e34be65218010dc3df1d1aa8e2c94af37b91b6
Nirmala <nirmala8579@gmail.com>
2025-09-09 05:51:04 +0530

    02.js

---
+++ 02.js
@@ -0,0 +1,16 @@
+let name = "Gowda";
+let age = 20;
+const pi = 3.14159;
+var city = "Bengaluru";
+
+console.log(name);
+console.log(age);
+console.log(pi);
+console.log(city);
+
+
+// OUTPUT---
+Gowda
+20
+3.14159
+Bengaluru
