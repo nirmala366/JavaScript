8cb5458c3c7d178e15a9e06684cdb8b9d0caf1c4
Nirmala <nirmala8579@gmail.com>
2025-09-10 06:37:23 +0530

    05.js

---
+++ 05.js
@@ -0,0 +1,31 @@
+// COMPARISON OPERATOR 
+
+let x = 10;
+let y = 5;
+
+// Equal to
+console.log("x == y:", x == y);
+
+// Not equal to
+console.log("x != y:", x != y);
+
+// Greater than
+console.log("x > y:", x > y);
+
+// Less than
+console.log("x < y:", x < y);
+
+// Greater than or equal to
+console.log("x >= y:", x >= y);
+
+// Less than or equal to
+console.log("x <= y:", x <= y);
+
+//OUTPUT 
+
+x == y: false
+x != y: true
+x > y: true
+x < y: false
+x >= y: true
+x <= y: false
