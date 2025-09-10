9562218715ab39b5620bb00642be869c68e295b0
Nirmala <nirmala8579@gmail.com>
2025-09-10 06:39:22 +0530

    06.js

---
+++ 06.js
@@ -0,0 +1,21 @@
+// LOGICAL OPERATOR 
+
+let a = true;
+let b = false;
+
+// AND (&&)
+console.log("a && b:", a && b);
+
+// OR (||)
+console.log("a || b:", a || b);
+
+// NOT (!)
+console.log("!a:", !a);
+console.log("!b:", !b);
+
+// OUTPUT 
+
+a && b: false
+a || b: true
+!a: false
+!b: true
