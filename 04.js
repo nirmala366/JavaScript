f559c9d1275f7b66ec4bd5cfbfff6d929430ff0f
Nirmala <nirmala8579@gmail.com>
2025-09-10 06:35:55 +0530

    04.js

---
+++ 04.js
@@ -0,0 +1,24 @@
+let a = 10;
+let b = 3;
+
+// Addition
+console.log("Addition:", a + b);
+
+// Subtraction
+console.log("Subtraction:", a - b);
+
+// Multiplication
+console.log("Multiplication:", a * b);
+
+// Division
+console.log("Division:", a / b);
+
+// Modulus (remainder)
+console.log("Modulus:", a % b);
+
+//OUTPUT ---
+Addition: 13
+Subtraction: 7
+Multiplication: 30
+Division: 3.3333333333333335
+Modulus: 1
