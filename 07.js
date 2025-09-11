a93b47b3f235bd8af130fac3801915b264eb727a
Nirmala <nirmala8579@gmail.com>
2025-09-11 05:42:44 +0530

    07.js

---
+++ 07.js
@@ -0,0 +1,30 @@
+// Bitwise operator ---
+
+let x = 5;   // (binary 0101)
+let y = 3;   // (binary 0011)
+
+// AND
+console.log("x & y:", x & y);
+
+// OR
+console.log("x | y:", x | y);
+
+// XOR
+console.log("x ^ y:", x ^ y);
+
+// NOT
+console.log("~x:", ~x);
+
+// Left Shift
+console.log("x << 1:", x << 1);
+
+// Right Shift
+console.log("x >> 1:", x >> 1);
+
+// OUTPUT ---
+x & y: 1
+x | y: 7
+x ^ y: 6
+~x: -6
+x << 1: 10
+x >> 1: 2
