fcc46e088de59bd08734a579d2d9d8753ee388a4
Nirmala <nirmala8579@gmail.com>
2025-09-12 06:31:14 +0530

    Shift OR Unshift.js

---
+++ Shift OR Unshift.js
@@ -0,0 +1,21 @@
+// Shift ---
+
+let fruits = ["Apple", "Banana", "Mango"];
+fruits.shift();
+
+console.log(fruits);
+
+// OUTPUT ---
+
+["Banana", "Mango"]
+
+// Unshift---
+
+let fruits = ["Banana", "Mango"];
+fruits.unshift("Apple");
+
+console.log(fruits);
+
+// OUTPUT ---
+
+["Apple", "Banana", "Mango"]
