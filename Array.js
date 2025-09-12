9a0aca38627259e6b35512322ec5079929251d30
Nirmala <nirmala8579@gmail.com>
2025-09-12 06:24:17 +0530

    Array.js

---
+++ Array.js
@@ -0,0 +1,17 @@
+let fruits = ["Mango", "Apple", "Banana"];
+
+console.log("First fruit:", fruits[0]);
+console.log("Second fruit:", fruits[1]);
+
+// Looping through array
+for (let i = 0; i < fruits.length; i++) {
+  console.log("Fruit:", fruits[i]);
+}
+
+// OUTPUT ---
+
+First fruit: Mango
+Second fruit: Apple
+Fruit: Mango
+Fruit: Apple
+Fruit: Banana
