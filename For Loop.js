bdc243ca979eebb41306e785431b2140f7681d25
Nirmala <nirmala8579@gmail.com>
2025-09-12 06:17:46 +0530

    Forloop.js

---
+++ For Loop.js
@@ -0,0 +1,10 @@
+for (let i = 1; i <= 5; i++) {
+  console.log("For Loop Count:", i);
+}
+
+// OUTPUT ---
+For Loop Count: 1
+For Loop Count: 2
+For Loop Count: 3
+For Loop Count: 4
+For Loop Count: 5
