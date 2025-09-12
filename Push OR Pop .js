df4e5f26b6d3a3334535a07866f872a8db0d4ed4
Nirmala <nirmala8579@gmail.com>
2025-09-12 06:29:02 +0530

    Push OR Pop .js

---
+++ Push OR Pop .js
@@ -0,0 +1,20 @@
+// PUSH ---
+let fruits = ["Apple", "Banana"];
+fruits.push("Mango");
+
+console.log(fruits);
+
+//OUTPUT ---
+
+["Apple", "Banana", "Mango"]
+
+
+// POP---
+
+let fruits = ["Apple", "Banana", "Mango"];
+fruits.pop();
+
+console.log(fruits);
+
+// OUTPUT ---
+["Apple", "Banana"]
