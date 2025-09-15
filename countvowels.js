6fe154567dab1bfb527ab1cfcdcb64daa54cdf6e
Nirmala <nirmala8579@gmail.com>
2025-09-15 17:12:49 +0530

    countvowels.js

---
+++ countvowels.js
@@ -0,0 +1,17 @@
+function countVowels(str) {
+  let vowels = "aeiouAEIOU";
+  let count = 0;
+
+  for (let char of str) {
+    if (vowels.includes(char)) {
+      count++;
+    }
+  }
+  return count;
+}
+
+console.log(countVowels("JavaScript")); // 3
+
+
+// output---
+3
