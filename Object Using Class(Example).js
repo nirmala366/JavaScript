4128ca825ffc1fa573d6a64792b83d138528c24f
Nirmala <nirmala8579@gmail.com>
2025-09-16 06:06:21 +0530

    Object Using Class(Example).js

---
+++ Object Using Class(Example).js
@@ -0,0 +1,7 @@
+// Creating objects from the Car class
+let car1 = new Car("Toyota", "Innova");
+let car2 = new Car("Tesla", "Model 3");
+
+// Calling methods on objects
+car1.displayInfo();
+car2.displayInfo();
