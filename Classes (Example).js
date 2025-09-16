d765ad4f50fb61acb0f2a31a5abbd703ddf4d7f5
Nirmala <nirmala8579@gmail.com>
2025-09-16 06:05:34 +0530

    Classes (Example).js

---
+++ Classes (Example).js
@@ -0,0 +1,11 @@
+// Defining a class
+class Car {
+  constructor(brand, model) {
+    this.brand = brand;
+    this.model = model;
+  }
+
+  displayInfo() {
+    console.log("Car:", this.brand, this.model);
+  }
+}
